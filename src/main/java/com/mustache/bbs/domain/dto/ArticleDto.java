0f955bcfe2a5387832457523f7025304cd8ca1e8
sujin0607 <goweg0607@gmail.com>
2022-11-07 14:25:23 +0900

    feat: ArticleController, ArticleDto, new template 추가

---
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -0,0 +1,19 @@
+package com.mustache.bbs.domain.dto;
+
+public class ArticleDto {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public ArticleDto(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString(){
+        return "ArticleDto{"+
+                "title='"+title+'\''+",content="+content+'\''+'}';
+    }
+}
